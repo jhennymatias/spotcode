5e4fbc76ac956e50aafb7ebcdd444c209a18f541
Jhennifer Matias <jhennifercristinematias@gmail.com>
2020-04-21 16:25:46 -0300

    dados enviados

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,8 +1,10 @@
 class Album < ApplicationRecord
   belongs_to :category
   belongs_to :artist
-  has_many: song
-  validates: title, presence:true
-  validates: date, presence:true
-  has_one_attached:cover
+  has_many :songs
+ 
+  validates :title, presence: true
+  validates :date, presence: true
+ 
+  has_one_attached :cover
 end
