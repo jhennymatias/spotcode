05b1221b4cb2b891917afa97398dd455ded8098e
Jhennifer Matias <jhennifercristinematias@gmail.com>
2020-04-22 19:36:17 -0300

    backend terminado

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -2,4 +2,7 @@ class Api::V1::CategoriesController < ApplicationController
     def index
       @categories = Category.all
     end
+    def show
+      @category = Category.find(params[:id])
+    end
   end
