684b033ab1a2f7fa3ddb07d851800855122572da
Jhennifer Matias <jhennifercristinematias@gmail.com>
2020-04-23 18:57:39 -0300

    incluindo bulma

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
- 
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Button } from 'react-bulma-components';
+import 'app.scss'
 const App = () => {
   return(
     <Fragment>
-      Hello
+      <Button color="primary">Testando o Bulma</Button>
     </Fragment>
   );
 }
