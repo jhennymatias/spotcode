ce81938e17bfc3bd3d23ccc6283bb521518efaee
Jhennifer Matias <jhennifercristinematias@gmail.com>
2020-04-24 20:49:09 -0300

    navbar finalizada

--- app/javascript/components/common/menu/index.js
+++ app/javascript/components/common/menu/index.js
@@ -1,41 +1,30 @@
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-	
 import React, { Fragment } from 'react';
-import { Navbar, Container, Columns } from 'react-bulma-components';
- 
+import { Navbar, Container, Columns, Button } from 'react-bulma-components';
+import logoImage from '../../assets/images/logo.png'
+import styled from 'styled-components'
+
+const ColumnsFullWidth = styled(Columns)`
+  width: 100%;
+  padding-top: 20px;
+  padding-left: 10px;
+  padding-bottom: 10px;
+`
+
 const Menu = () => {
   return (
     <Fragment>
       <Navbar color="dark">
         <Container>
-   		  <Columns className='is-mobile'>
+   		  <ColumnsFullWidth className='is-mobile'>
             <Columns.Column desktop={{size: 2}} mobile={{size: 5}}>
-            <div>jhenny</div>
+              <img src={logoImage} className='image'/>
             </Columns.Column>
             <Columns.Column>
-              
+              <a href='/users/sign_in' className="is-pulled-right is-right">
+                <Button outlined={true} color="white">ENTRAR</Button>
+              </a>
             </Columns.Column>
-          </Columns>
+          </ColumnsFullWidth>
         </Container>
       </Navbar>
     </Fragment>
