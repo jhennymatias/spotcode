f2257caf57666b4c4758c87d6e23f7ae185208fa
Jhennifer Matias <jhennifercristinematias@gmail.com>
2020-04-27 14:07:09 -0300

    discovery-layout

--- app/javascript/screens/discovery/index.js
+++ app/javascript/screens/discovery/index.js
@@ -1,19 +1,11 @@
 import React, {Fragment} from 'react';
 import NavbarFooter from '../../components/common/navbar_footer';
 import Disco from '../../components/discovery';
-import Album from '../album';
-
-import styled from 'styled-components';
- 
-const DivVSpaced = styled.div`
-  margin-top: 50px;
-`
 
 const Discovery = () =>{
     return(
         <Fragment>
-            <Disco/>
-            <Album/>            
+            <Disco/>         
             <NavbarFooter/>
         </Fragment>
     )
